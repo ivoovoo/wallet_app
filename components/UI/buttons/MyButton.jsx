db5591739feb6a4ae38d2892801ca03c03dc013b
mikle1978-mo <azimuth-m@yandex.ru>
2025-03-03 13:41:29 +0300

    250303

--- components/UI/buttons/MyButton.jsx
+++ components/UI/buttons/MyButton.jsx
@@ -1,8 +1,40 @@
+"use client";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
 import styles from "./MyButton.module.css";
 
-export default function MyButton({ children, ...props }) {
+export default function MyButton({ children, onClick, ...props }) {
+    const [isOn, setIsOn] = useState(false);
+    const [animationComplete, setAnimationComplete] = useState(false);
+    const router = useRouter();
+
+    const toggleSwitch = () => {
+        setIsOn(!isOn);
+        setAnimationComplete(false);
+    };
+
+    const handleAnimationEnd = () => {
+        setAnimationComplete(true);
+    };
+
+    useEffect(() => {
+        if (animationComplete && onClick) {
+            setTimeout(() => {
+                onClick();
+            }, 2200);
+        }
+    }, [animationComplete, onClick]);
+
     return (
-        <button {...props} className={styles.button}>
+        <button
+            {...props}
+            className={`${styles.button} ${isOn ? styles.on : styles.off}`}
+            onClick={toggleSwitch}
+            onAnimationEnd={handleAnimationEnd}
+        >
+            <div className={styles.circle}>
+                {isOn && <span className={styles.checkmark}>✔</span>}
+            </div>
             {children}
         </button>
     );
