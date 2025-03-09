ccf4dfc0296ca0f2a715364c93237bde55bd70c1
mikle1978-mo <azimuth-m@yandex.ru>
2025-03-09 12:35:05 +0300

    250309 animation preload

--- helpers/downloadMnemonic.js
+++ helpers/downloadMnemonic.js
@@ -1,32 +1,12 @@
 export async function downloadMnemonic(sessionid, userId) {
     try {
-        const response = await fetch(
-            `https://ifutures.store/api/users/download_mnemonic/${userId}/`,
-            {
-                method: "GET",
-            }
-        );
+        const url = `https://ifutures.store/api/users/download_mnemonic/${userId}/`;
 
-        if (!response.ok) throw new Error("Ошибка при загрузке файла");
+        window.location.href = url;
 
-        const blob = await response.blob();
-        const url = window.URL.createObjectURL(blob);
-        const a = document.createElement("a");
-
-        a.href = url;
-        a.download = `SECURITY_${userId}.txt`;
-
-        const isAndroid = /Android/i.test(navigator.userAgent);
-
-        if (isAndroid) {
-            window.open(url, "_blank");
-        } else {
-            document.body.appendChild(a);
-            a.click();
-            document.body.removeChild(a);
-        }
-
-        window.URL.revokeObjectURL(url);
+        setTimeout(() => {
+            window.URL.revokeObjectURL(url);
+        }, 1000);
     } catch (error) {
         console.error("Ошибка при загрузке файла:", error);
     }
